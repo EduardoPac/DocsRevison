f64f5b8690679c8023a4ea68198bab17cf9a4235
Eduardo <eduardo@forlogic.net>
2019-11-23 09:21:57 -0300

    Criando projeto do TCC

---
+++ DocsRevision/DocsRevision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DocsRevision
+//
+//  Created by Eduardo-Forlogic on 23/11/19.
+//  Copyright © 2019 Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
