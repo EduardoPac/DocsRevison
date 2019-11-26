8d8b4ffea0d8f27bc87c944cf550a138c5ed30d4
Eduardo <eduardo@forlogic.net>
2019-11-25 21:21:25 -0300

    Criando entidades da api

---
+++ DocsRevision/DocsRevision.API/DocsAPI/Entities/Revision.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DocsAPI.Enum;
+
+namespace DocsAPI.Entities
+{
+    public class Revision
+    {
+        public string Id { get; set; }
+        public string DocumentId { get; set; }
+        public string NumRevision { get; set; }
+        public string RevisorId { get; set; }
+        public User Revisor { get; set; }
+        public string ApproverId { get; set; }
+        public User Approver { get; set; }
+        public EStatus Status { get; set; }
+        public DateTimeOffset? RevisonDate { get; set; }
+        public DateTimeOffset ExpirationDate { get; set; }
+        public DateTimeOffset? DeadlineDate { get; set; }
+        public bool IsObsolete { get => ExpirationDate > DateTimeOffset.Now; }
+    }
+}
---
+++ DocsRevision/DocsRevision.API/DocsAPI/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DocsAPI.Entities
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
