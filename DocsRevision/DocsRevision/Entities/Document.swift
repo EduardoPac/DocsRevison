9226a09955874a511068a3614f9d05bb583c4d16
Eduardo <eduardo@forlogic.net>
2019-11-27 20:41:31 -0300

    Criando layout das telas completo

--- DocsRevision/DocsRevision/Entities/Document.swift
+++ DocsRevision/DocsRevision/Entities/Document.swift
@@ -4,6 +4,7 @@ class Document{
     var Id: String
     var Name: String
     var CreationDate: NSDate
+    var DocType : EType
     var CreationId: String
     var RevisionId: String
     var Revision: Revision
@@ -13,13 +14,22 @@ class Document{
         name: String,
         creationDate: NSDate,
         creationId: String,
+        docType : EType,
         revisionId: String,
         revision: Revision) {
         self.Id=id
         self.Name=name
+        self.DocType = docType
         self.CreationDate=creationDate
         self.CreationId=creationId
         self.RevisionId=revisionId
         self.Revision=revision
     }
+}
+
+enum EType {
+    case pdf
+    case doc
+    case ppt
+    case xls
 }
